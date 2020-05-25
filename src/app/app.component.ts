252b94f1a221e529aa581d11bae186610b61a8e7
djonnyx <djonnyx@gmail.com>
2020-05-25 16:34:53 +0300

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+
+export enum Themes {
+  LIGHT = "light",
+  DARK = "dark"
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'ng-ui-kit-demo';
+export class AppComponent implements OnInit {
+  currentTheme: Themes = Themes.LIGHT;
+
+  constructor(@Inject(DOCUMENT) private document: Document) {}
+
+  ngOnInit() {
+    this.themeLoad();
+  }
+
+  themeChange() {
+    this.currentTheme = this.currentTheme === Themes.LIGHT ? Themes.DARK : Themes.LIGHT;
+    this.themeLoad();
+  }
+
+  themeLoad() {
+    if (this.currentTheme === Themes.LIGHT) {
+      import(
+        /* webpackMode: "lazy" */
+        '../styles/themes/theme-light.scss' as any);
+    } else if (this.currentTheme === Themes.DARK) {
+      import(
+        /* webpackMode: "lazy" */
+        '../styles/themes/theme-dark.scss' as any);
+    }
+  }
 }
